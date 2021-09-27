70df1bd6fae090bd697c1378a4ec34c950078782
Francisco Emanoel Ferreira da Penha <francisco1code@gmail.com>
2021-09-27 18:52:19 -0300

    Adicionando solução da torre de hanoi

--- src/solucao-torre.py
+++ src/solucao-torre.py
@@ -1,1 +1,29 @@
-print("teste")
+import sys
+nmov=0
+subconjuntos=[]
+
+
+def move(n,torre1,torre3,conj):
+        global nmov
+        nmov+=1
+        print ("Mov numero: ", nmov,"move disco:",n," de:",torre1,"-->",torre3)
+
+def hanoi(n,torre1,torre3,torre2,conj):
+    if(n==1):
+        move(n,torre1,torre3,conj)
+    else:
+        hanoi(n-1,torre1,torre2,torre3,conj)
+        move(n,torre1,torre3,conj)
+        hanoi(n-1,torre2,torre3,torre1,conj)
+
+    
+if __name__ == '__main__':
+
+    print("Digite a quantidade de discos: ")
+    ndiscos= int(input())
+   
+    conj=[]
+    subconjuntos.append([])
+    for i in range(0,ndiscos):
+        conj.append(0)
+    hanoi(ndiscos,'A','B','C',conj)
